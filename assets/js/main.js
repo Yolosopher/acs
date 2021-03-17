accc1220b3a2474965ec6553cb631ba0d15dcae0
Yolosopher <nika.nishnianidze.10@gmail.com>
2021-03-17 22:42:32 +0400

    infowindow + functionallity added

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,20 +8,7 @@ burgerContainer.addEventListener('click', () => {
     body.classList.toggle('noscroll')
 })
 
-const cells = document.querySelectorAll('.periodic__content__row__cell')
 
-
-cells.forEach(cell => {
-    cell.addEventListener('click', () => {
-        let td = cell.closest('td')
-        if (cell.classList.contains('shown')) {
-            cell.classList.remove('shown')
-        } else {
-            cells.forEach(each => each.classList.remove('shown'))
-            cell.classList.add('shown')
-        }
-    })
-})
 
 
 
